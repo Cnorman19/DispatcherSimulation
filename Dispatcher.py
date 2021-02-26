2423e6c1a2925a3ec836fa80c40b4d448be1eaff
Cameron Norman <cnorm001@Odu.edu>
2021-02-26 11:05:53 -0500

    Add files via upload

--- Dispatcher.py
+++ Dispatcher.py
@@ -1,10 +1,19 @@
+# Importing necessary Dependencies
 from queue import PriorityQueue
 from functools import total_ordering
+import itertools
+from random import randrange
 
 # Process Object to add to ReadyQueue and BlockedList
 @total_ordering
 class Process:
+
+	# Auto Iterating ID
+	newid = itertools.count()
+
+	# Overloading Dunder Methods
 	def __init__(self, name, status, priority):
+		self.id = next(Process.newid)
 		self.name = name
 		self.status = status 
 		self.priority = priority
@@ -16,37 +25,89 @@ class Process:
 		return self.priority == other.priority
 
 	def __str__(self):
-		return self.name + " - " + " Status: " + self.status + " - " + " Priority: " + self.priority
+		return "ID: " + str(self.id) + " - " + self.name + " - " + " Status: " + self.status + " - " + " Priority: " + self.priority
+
+		
 
 class Dispatcher:
 	
 	# Initialize ReadyQueue and BlockedList Priority Queues
 	ReadyQueue = PriorityQueue()
 	BlockedList = PriorityQueue()
+	RunningQueue = PriorityQueue()
 
 	# Add processes to ReadyQueue and BlockedList
-	def addProcess(cls):
+	def populateReadyQueue(cls):
 		P1 = Process("Process 1", "Ready", '1')
-		P2 = Process("Process 2", "Working", '2')
-		P3 = Process("Process 3", "Terminated", '3')
+		P2 = Process("Process 2", "Ready", '2')
 
-		B1 = Process("Blocked Process 1", "BLOCKED", '3')
-		B2 = Process("Blocked Process 2", "BLOCKED", '1')
+		R1 = Process("Process 3", "Runnning", '1')
+
+		B1 = Process("Process 4", "Blocked", '3')
+		B2 = Process("Process 5", "Blocked", '1')
 
 		cls.ReadyQueue.put(P1)
 		cls.ReadyQueue.put(P2)
-		cls.ReadyQueue.put(P3)
+
+		cls.RunningQueue.put(R1)
 
 		cls.BlockedList.put(B1)
 		cls.BlockedList.put(B2)
 	
-	# Print contents of ReadyQueue
-	def printQueues(cls):
-		while not cls.ReadyQueue.empty():
-			next_item = cls.ReadyQueue.get()
-			print(next_item)
+	# Remove Process from ReadyQueue
+	def removeProcess(cls):
+		temp = cls.ReadyQueue.get()
+		print("Process Terminated -> " + str(temp) + "\n")
+		cls.printQueues() 
 
+
+	# Switch status of Process
+	def contextSwitch(cls):
+		current = cls.ReadyQueue.get()
+		current.status = "Running"
+		cls.RunningQueue.put(current)
+
+		current2 = cls.RunningQueue.get()
+		current2.status = "Blocked"
+		cls.BlockedList.put(current2)
+
+		x = randrange(2)
+		
+		if x == 0: 
+			print("Context Switch Initiated - Cause: Time Slice Exceeded!\n")
+
+		if x == 1:
+			print("Context Switch Initiated - Cause: Blocking Call! \n")
+
+		cls.printQueues()
+
+	# Print contents of both queues
+	def printQueues(cls):
+
+		# Iterators for print loop(s)
+		indexReadyQueue,indexBlockedList,indexRunningQueue = 0,0,0
+
+		# Print contents of ReadyQueue
+		print("Ready Processes:")
+		while indexReadyQueue != cls.ReadyQueue.qsize():
+			print(cls.ReadyQueue.queue[indexReadyQueue])
+			indexReadyQueue+=1
+
+		# Print contents of RunningQueue
+		print("Running Processes:")
+		while indexRunningQueue != cls.RunningQueue.qsize():
+			print(cls.RunningQueue.queue[indexRunningQueue])
+			indexRunningQueue+=1
+			
 		# Print contents of BlockedList
-		while not cls.BlockedList.empty():
-			next_blocked_item = cls.BlockedList.get()
-			print(next_blocked_item)
+		print("Blocked Processes:")
+		while indexBlockedList != cls.BlockedList.qsize():
+			print(cls.BlockedList.queue[indexBlockedList])
+			indexBlockedList+=1
+		print("\n")
+
+D = Dispatcher()
+D.populateReadyQueue()
+D.printQueues()
+D.contextSwitch()
+D.removeProcess()
